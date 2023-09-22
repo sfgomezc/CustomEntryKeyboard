73499b0cfc71b07029c93ea9b0026231d5abbe2e
Steven Gomez <steven.gomez.c@gmail.com>
2023-09-22 11:50:00 -0500

    Switch the focus to the next entry control using keyboard

--- NextDoneButton/NextDoneButton/MainPage.xaml.cs
+++ NextDoneButton/NextDoneButton/MainPage.xaml.cs
@@ -16,5 +16,15 @@ namespace NextDoneButton
             InitializeComponent();
             BindingContext = new LoginViewModel();
         }
+
+        private void Handle_Completed(object sender, System.EventArgs e)
+        {
+            var tabs = Layout.GetTabIndexesOnParentPage(out int count);
+            var visual = sender as Xamarin.Forms.VisualElement;
+            var currentIndex = visual.TabIndex;
+            var nextFocus = Layout.FindNextElement(true, tabs, ref currentIndex);
+
+            (nextFocus as Xamarin.Forms.VisualElement)?.Focus();
+        }
     }
 }
